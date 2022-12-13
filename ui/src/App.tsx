41b6368747ea644bd488bc562f9e245da1ab3f51
Valyn Hall <valynhall@gmail.com>
2022-12-13 14:10:31 -0500

    Added UI

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
